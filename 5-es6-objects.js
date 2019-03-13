9f3d5ad6e5c842ad00a9b28ea7af2d39eb8d57ec
Andres Alba <andres_aalba@hotmail.com>
2019-03-12 21:49:35 -0500

    Add object property short-hand and destructuting examples (Lecture 39)

---
+++ 5-es6-objects.js
@@ -0,0 +1,32 @@
+// Object property short-hand
+const name = 'Andres';
+
+const userAge = 25;
+
+const user = {
+    name,
+    age: userAge,
+    location: 'Panama'
+};
+
+console.log(user);
+
+// Object desctructuring
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined,
+    rating: 4.2
+};
+
+const {label:productLabel, stock, rating = 5} = product;
+console.log(productLabel);
+console.log(stock);
+console.log(rating);
+
+const transaction = (type, {label, stock}) => {
+    console.log(type, label, stock);
+};
+
+transaction('order', product);
